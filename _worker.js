75bb717d01a5c6883ae5bb7a721afbf67e838b9d
xtapo <40712625+xtapo@users.noreply.github.com>
2025-01-28 09:38:31 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
     async fetch(request, env) {
       let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
+      if (url.pathname.startsWith('/gh/nhanhq1038/photos-xtapo/')) {
         url.hostname="cdn.jsdelivr.net";
         let new_request=new Request(url,request);
         return fetch(new_request);
